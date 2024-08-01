f4a5a9219588be433853470ddd33631a283f5c43
Srinu2023mca <132866061+Srinu2023mca@users.noreply.github.com>
2024-08-01 10:26:44 +0530

    done

--- index.js
+++ index.js
@@ -18,5 +18,9 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use('/api/media', mediaRoutes);
 
+app.get("/",(req,res)=>{
+    res.send("API running")
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
